c491fb16274cf63c338c511dae210e4b49324967
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-07-26 18:07:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+document.addEventListener('DOMContentLoaded', function() {
+    const getSumBtn = document.createElement("button");
+    getSumBtn.append("Get Total Price");
+    document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-//Add your code here
-  
-};
+    const getSum = () => {
+        let prices = document.getElementsByClassName("price");
+        let sum = 0;
 
-getSumBtn.addEventListener("click", getSum);
+        for (let i = 0; i < prices.length; i++) {
+            sum += parseFloat(prices[i].textContent);
+        }
 
+        let totalPriceElement = document.querySelector(".total_price");
+        totalPriceElement.innerHTML = "Total Price: Rs " + sum;
+    };
+
+    getSumBtn.addEventListener("click", getSum);
+});
